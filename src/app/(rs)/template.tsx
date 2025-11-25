cc43237a56b8f0e5191ccbb83333448adef264c9
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2025-11-25 16:52:28 -0600

    In global.css, created utilties block for general utilities. Created two animation utilities in block. | Removed tw-animate-css from dependencies. | Updated meta data for project in layout.tsx of root folder.| Animation for header and page route load applied and working.

--- src/app/(rs)/template.tsx
+++ src/app/(rs)/template.tsx
@@ -1,4 +1,4 @@
-"For things that must re-render each time on (e.g. loading animation).";
+// For things that must re-render each time on (e.g. loading animation, page transitions, etc.).;
 export default async function Template({
   children,
 }: Readonly<{
